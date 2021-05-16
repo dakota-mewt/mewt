dbd66df1205e328430d304c346e50f2bbab185de
Dakota <billy.zhao@gmail.com>
2021-05-16 16:28:00 -0400

    adding mewt_quick.ps1

--- code/windows/mewt zip files/setup_mewt.ps1
+++ code/windows/mewt zip files/setup_mewt.ps1
@@ -17,6 +17,7 @@ cd $install_path
 New-Item $mewt_path -Type directory -Force
 Copy-Item ".\AudioDeviceCmdlets.dll" $mewt_path
 Copy-Item ".\mewt.ps1" $mewt_path 
+Copy-Item ".\mewt_quick.ps1" $mewt_path 
 Copy-Item ".\start_mewt.bat" $mewt_path
 Copy-Item ".\setup_mewt.bat" $mewt_path 
 Copy-Item ".\setup_mewt.ps1" $mewt_path 
